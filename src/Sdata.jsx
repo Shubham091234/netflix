7061dbee44ea81bd5d96584cb37a7ec3f323a549
Shubham091234 <shubhamsingh091234@gmail.com>
2023-08-23 18:36:32 +0530

    third commits

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,8 +1,5 @@
 const Sdata = [
-    { sources : "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg" ,
-      snames:"Stranger Things",
-      categorys:"Thriller and Sci-Fi",
-      links:" https://www.netflix.com/in/title/80057281" },
+   
 
       { sources : "https://assets.gadgets360cdn.com/pricee/assets/product/202202/MV5BZmY2YzU4NDktODIxYi00YWIyLWIz_1644909573.jpg" ,
       snames:"Dark",
@@ -13,16 +10,14 @@ const Sdata = [
       snames:"1899",
       categorys:"Thriller and Sci-Fi",
       links:"https://www.netflix.com/in/title/80214497 " },
+      
 
       { sources : "https://staticg.sportskeeda.com/editor/2022/01/0bab4-16413888728520-1920.jpg" ,
       snames:"Friends",
       categorys:"Comedy, Drama",
       links:"https://www.netflix.com/in/title/70153404 " },
 
-      { sources : "https://akm-img-a-in.tosshub.com/indiatoday/images/media_bank/202308/guns-and-gulaabs-183150-1x1.jpeg?VersionId=FdB1m4GQWe4uY8M3b73mnSMeFv9d30XM" ,
-      snames:"Gulabs and Guns",
-      categorys:"Thriller and Action",
-      links:"https://www.netflix.com/in/title/81313531 " },
+   
 
       { sources : "https://i0.wp.com/thsindex.org/wp-content/uploads/2023/04/Money-Heist-was-first-released-on-Antena-3-in-Spain-in-2017-Netflix.jpg?fit=1920%2C2560&ssl=1" ,
       snames:"Money Heist",
@@ -37,7 +32,15 @@ const Sdata = [
       { sources : "https://m.media-amazon.com/images/M/MV5BNDJjMzc4NGYtZmFmNS00YWY3LThjMzQtYzJlNGFkZGRiOWI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" ,
       snames:"Lucifer",
       categorys:"Romance and Drama",
-      links:"https://www.netflix.com/in/title/80057918 " }
+      links:"https://www.netflix.com/in/title/80057918 " },
+      { sources : "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg" ,
+      snames:"Stranger Things",
+      categorys:"Thriller and Sci-Fi",
+      links:" https://www.netflix.com/in/title/80057281" },
+      { sources : "https://akm-img-a-in.tosshub.com/indiatoday/images/media_bank/202308/guns-and-gulaabs-183150-1x1.jpeg?VersionId=FdB1m4GQWe4uY8M3b73mnSMeFv9d30XM" ,
+      snames:"Guns And Gulabs",
+      categorys:"Thriller and Action",
+      links:"https://www.netflix.com/in/title/81313531 " }
 
 ]
 
