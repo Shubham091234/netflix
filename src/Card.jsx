b4acd4545b8a5e61e3d85279324fde72acf883eb
Shubham091234 <shubhamsingh091234@gmail.com>
2023-08-23 19:09:34 +0530

    4th commits

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,7 +8,7 @@ function Card(props) {
         <img src={props.source} alt="" className="card_img" />
         <div className="card_info" >
 <span className="card_category">{props.category}</span>
-<h3 className="card_title">{props.sname}</h3>
+<h3 className="card_title" title={props.sname}>{props.sname}</h3>
 <a href={props.link} target="_blank">
     <button className="button">Watch Now</button>
 </a>
